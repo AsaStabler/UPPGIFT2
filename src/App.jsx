7e8419544a55a436ab199b6936d66d333a08c4e1
Åsa Stabler <asa.stabler@gmail.com>
2024-11-08 20:20:32 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+//import { useState } from 'react'
+
+
+//Alt 1: 
+/* 
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1> 
+    </>
+  )
+}   
+*/
+
+//Alt 2:
+const App = () => {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World!</h1> 
+      <p>Hello react!</p>
+      <div className="bg-primary">APP</div>
+    </>
+  )
+}
+
+export default App
